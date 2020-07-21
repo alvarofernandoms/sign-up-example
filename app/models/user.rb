ac7fa4831534ac2e874abbf814b25378f08f43f7
Álvaro Fernando <alvarofernandoms@gmail.com>
2020-07-20 21:35:57 -0300

    Add User validation on model and on front-end

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :password_digest, presence: true
+  validates :password_digest, length: { minimum: 8 }
+
+  validates :username, length: { minimum: 5 }, on: :update
 end
